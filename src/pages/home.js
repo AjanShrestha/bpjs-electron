a6b76aa6f4be5eb65f1aa65f4e7882a7caf57c39
AJRedDevil <ajan.shresh@gmail.com>
2017-12-16 17:45:55 +0545

    Add basic crunchyroll scraping API for series catalogue

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,46 @@
-import React from 'react';
+// npm packages
+import React, {Component} from 'react';
+import {Observable} from 'rxjs';
 
-export default () => (
-  <div>
-    <h1>Hello World!</h1>
-  </div>
-);
+// our packages
+import db from '../db';
+import {Crunchyroll} from '../api';
+import Series from '../components/series';
+
+export default class Home extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      series: [],
+    };
+
+    // trigger list update
+    Crunchyroll.getAllSeries();
+  }
+
+  componentDidMount() {
+    this.sub = Observable.fromEvent(
+      db.series.changes({
+        since: 0,
+        live: true,
+        include_docs: true,
+      }),
+      'change'
+    )
+      .filter(change => !change.deleted)
+      .map(change => change.doc)
+      .scan((acc, doc) => acc.concat([doc]), [])
+      .debounceTime(1000)
+      .subscribe(series => this.setState({series}));
+  }
+
+  componentWillUnmount() {
+    this.sub.unsubscribe();
+  }
+
+  render() {
+    const {series} = this.state;
+    return <div>{series.map(s => <Series key={s._id} series={s} />)}</div>;
+  }
+}
