51310c7f17b7b1d9ca5ff2ac118f59281d614130
AJRedDevil <ajan.shresh@gmail.com>
2017-12-19 06:56:03 +0545

    Add basic video playback (no streaming yet)

--- src/pages/episode.js
+++ src/pages/episode.js
@@ -1,29 +1,55 @@
 // npm packages
 import React, {Component} from 'react';
+import videojs from 'video.js';
 
 // our packages
 import {Crunchyroll} from '../api';
-import {LocationPropType} from '../utils';
 
 export default class Episode extends Component {
   constructor(props) {
     super(props);
-    console.log('Episode');
 
-    // this.state = {
-    //   episode: null,
-    // };
+    this.state = {
+      episode: null,
+      filename: null,
+    };
 
     // trigger episode loading
+    this.init(props);
+  }
+
+  componentDidUpdate() {
+    const {episode, filename} = this.state;
+
+    if (!episode || !filename) {
+      return;
+    }
+
+    videojs('video');
+  }
+
+  async init(props) {
     const {location} = props;
-    Crunchyroll.getEpisode(location.state);
+    const filename = await Crunchyroll.getEpisode(location.state);
+    this.setState({
+      episode: location.state,
+      filename,
+    });
   }
 
   render() {
-    return <div>episode video here</div>;
+    const {episode, filename} = this.state;
+
+    if (!episode || !filename) {
+      return <div>Loading...</div>;
+    }
+    return (
+      <div>
+        episode video here
+        <video id="video" className="video-js" controls autoPlay preload="auto">
+          <source src={filename} type="video/mp4" />
+        </video>
+      </div>
+    );
   }
 }
-Episode.propTypes = {
-  // eslint-disable-next-line react/no-typos
-  location: LocationPropType.isRequired,
-};
